7261cd4903038bf87930fd1a1638ead008250093
Youcef9897 <fettal@et.esiea.fr>
2024-12-23 20:31:56 +0100

    Update reproduction.c

--- essie/reproduction.c
+++ essie/reproduction.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include "structures.h"
-#include "fourmis.h"
-
-#include <stdio.h>
-#include <string.h>
-#include "structures.h"
+#include "reproduction.h"
 #include "fourmis.h"
 
 // Fonction pour gérer la reproduction de la reine
@@ -41,41 +36,4 @@ void reproductionReine(Fourmi *colonie, int *tailleColonie, GestionNonAdultes *n
     } else {
         printf("Aucun mâle actif disponible dans la colonie. Pas de reproduction possible.\n");
     }
-}
-
-void consommationNonAdultes(int nbOeufs, int nbLarves, int nbNymphes, StockNourriture *stockNourriture) {
-    float consommationTotale = 0.0;
-
-    // Consommation basée sur le ratio (1 unité pour 5 non-adultes)
-    float consommationOeufs = nbOeufs / 5.0;
-    float consommationLarves = nbLarves / 5.0;
-    float consommationNymphes = nbNymphes / 5.0;
-
-    consommationTotale += consommationOeufs;
-    consommationTotale += consommationLarves;
-    consommationTotale += consommationNymphes;
-
-    // Répartition équitable de la consommation sur les ressources
-    float consommationParRessource = consommationTotale / 4.0;
-
-    // Réduction des ressources en vérifiant la disponibilité
-    stockNourriture->sucre = (stockNourriture->sucre >= consommationParRessource) ?
-                             (stockNourriture->sucre - consommationParRessource) : 0;
-
-    stockNourriture->champignons = (stockNourriture->champignons >= consommationParRessource) ?
-                                   (stockNourriture->champignons - consommationParRessource) : 0;
-
-    stockNourriture->proteines = (stockNourriture->proteines >= consommationParRessource) ?
-                                 (stockNourriture->proteines - consommationParRessource) : 0;
-
-    stockNourriture->graines = (stockNourriture->graines >= consommationParRessource) ?
-                               (stockNourriture->graines - consommationParRessource) : 0;
-
-    // Affichage des informations sur la consommation
-    printf("\n--- Consommation des non-adultes ---\n");
-    printf(" - %d œufs consomment %.2f unités\n", nbOeufs, consommationOeufs);
-    printf(" - %d larves consomment %.2f unités\n", nbLarves, consommationLarves);
-    printf(" - %d nymphes consomment %.2f unités\n", nbNymphes, consommationNymphes);
-    printf("Consommation totale : %.2f unités réparties équitablement.\n", consommationTotale);
-    printf("------------------------------------------\n");
 }
