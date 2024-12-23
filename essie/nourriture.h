50159ed821d6aad852a501a6ec37ef5d8ad7f638
Youcef9897 <fettal@et.esiea.fr>
2024-12-23 20:34:30 +0100

    Update nourriture.h

--- essie/nourriture.h
+++ essie/nourriture.h
@@ -4,7 +4,20 @@
 #include "structures.h"
 #include "fourmis.h" // Inclusion pour accéder aux fonctions des fourmis
 
-void collecteRessources(Fourmi *colonie, int *taille, StockNourriture *stockNourriture, StockMateriaux *stockMateriaux, const char *saison);
-void calculerConsommation(Fourmi *colonie, int tailleColonie, GestionNonAdultes *nonAdultes, GestionConsommation *consommation);
-void consommationRessources(Fourmi *colonie, int tailleColonie, StockNourriture *stockNourriture, GestionNonAdultes *nonAdultes);
+// Fonction pour gérer la collecte des ressources
+void collecteRessources(Fourmi *colonie, int *taille, 
+                        StockNourriture *stockNourriture, 
+                        StockMateriaux *stockMateriaux, 
+                        const char *saison);
+    
+// Fonction pour calculer la consommation de nourriture
+void calculerConsommation(Fourmi *colonie, int tailleColonie, 
+                          GestionNonAdultes *nonAdultes, 
+                          GestionConsommation *consommation);
+
+// Fonction pour gérer la consommation des ressources
+void consommationRessources(Fourmi *colonie, int tailleColonie, 
+                             StockNourriture *stockNourriture, 
+                             GestionNonAdultes *nonAdultes);
+
 #endif // NOURRITURE_H
