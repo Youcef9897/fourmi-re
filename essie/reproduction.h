a1cede2af0cb54d8bfa032c0e5dcc1efffb1b746
Youcef9897 <fettal@et.esiea.fr>
2024-12-23 20:32:10 +0100

    Update reproduction.h

--- essie/reproduction.h
+++ essie/reproduction.h
@@ -2,10 +2,8 @@
 #define REPRODUCTION_H
 
 #include "structures.h"
-#include "fourmis.h"
 
-// Prototypes des fonctions
+// Déclaration des fonctions
 void reproductionReine(Fourmi *colonie, int *tailleColonie, GestionNonAdultes *nonAdultes, Climat *temps);
-void consommationNonAdultes(int nbOeufs, int nbLarves, int nbNymphes, StockNourriture *stockNourriture);
 
 #endif // REPRODUCTION_H
