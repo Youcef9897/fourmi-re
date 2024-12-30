153782526a5c2228c1d8f30b8c61d1f36c2f932c
Youcef9897 <fettal@et.esiea.fr>
2024-12-31 00:21:07 +0100

    Update reproduction.h

--- essie/reproduction.h
+++ essie/reproduction.h
@@ -3,7 +3,19 @@
 
 #include "structures.h"
 
-// Déclaration des fonctions
-void reproductionReine(Fourmi *colonie, int *tailleColonie, GestionNonAdultes *nonAdultes, Climat *temps);
+// Fonction pour gérer la reproduction dans la fourmilière
+void reproduction(Fourmi *colonie, int *tailleColonie, GestionNonAdultes *gestionNonAdultes, StockNourriture *stockNourriture, int nbOeufs);
+
+// Fonction pour déposer des œufs dans la colonie
+void deposerOeufs(Fourmi *colonie, int *tailleColonie, int nbOeufs);
+
+// Fonction pour faire évoluer les non-adultes (œufs -> larves -> nymphes -> adultes)
+void evoluerNonAdultes(Fourmi *colonie, int *tailleColonie, GestionNonAdultes *gestionNonAdultes);
+
+// Fonction pour afficher les états des non-adultes (œufs, larves, nymphes)
+void afficherNonAdultes(GestionNonAdultes *gestionNonAdultes);
+
+// Fonction pour consommer des ressources pour les non-adultes (œufs, larves, nymphes)
+void consommerRessourcesNonAdultes(GestionNonAdultes *gestionNonAdultes, StockNourriture *stockNourriture);
 
 #endif // REPRODUCTION_H
