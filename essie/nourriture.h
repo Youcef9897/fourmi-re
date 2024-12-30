b1b851e87df34db936bc52f88a7b128d18336926
Youcef9897 <fettal@et.esiea.fr>
2024-12-31 00:20:37 +0100

    Update nourriture.h

--- essie/nourriture.h
+++ essie/nourriture.h
@@ -4,18 +4,18 @@
 #include "structures.h"
 #include "fourmis.h" // Inclusion pour accéder aux fonctions des fourmis
 
-// Fonction pour gérer la collecte des ressources
+// Fonction pour gérer la collecte des ressources, y compris le miel
 void collecteRessources(Fourmi *colonie, int *taille, 
                         StockNourriture *stockNourriture, 
                         StockMateriaux *stockMateriaux, 
                         const char *saison);
-    
-// Fonction pour calculer la consommation de nourriture
+
+// Fonction pour calculer la consommation de nourriture, avec prise en compte des pots de miel
 void calculerConsommation(Fourmi *colonie, int tailleColonie, 
                           GestionNonAdultes *nonAdultes, 
                           GestionConsommation *consommation);
 
-// Fonction pour gérer la consommation des ressources
+// Fonction pour gérer la consommation des ressources, en intégrant le miel
 void consommationRessources(Fourmi *colonie, int tailleColonie, 
                              StockNourriture *stockNourriture, 
                              GestionNonAdultes *nonAdultes);
