faf08a456b0682b774488d17d460e852d531c052
Youcef9897 <fettal@et.esiea.fr>
2025-01-09 01:07:05 +0100

    Update and rename nourriture.h to Ressources.h

---
+++ essie/Ressources.h
@@ -0,0 +1,16 @@
+#ifndef NOURRITURE_H
+#define NOURRITURE_H
+
+#include "structures.h"
+#include "FourmisColonie.h" // Inclusion pour accéder aux fonctions des fourmis
+
+// Fonction pour collecter de la nourriture par les fourmis
+void collecteNourriture(Fourmi *colonie, int taille, StockNourriture *stockNourriture, const char *saison);
+
+// Fonction pour collecter des matériaux par les fourmis
+void collecteMateriaux(Fourmi *colonie, int taille, StockMateriaux *stockMateriaux, Colonie *etatColonie);
+
+// Fonction pour gérer la consommation de nourriture par les fourmis
+void consommationNourriture(Fourmi *colonie, int tailleColonie, StockNourriture *stockNourriture);
+
+#endif // NOURRITURE_H
