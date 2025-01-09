e6a3825b5642b9a8231f0f4d3498b55180983110
Youcef9897 <fettal@et.esiea.fr>
2025-01-09 01:02:07 +0100

    Update combat.h

--- essie/combat.h
+++ essie/combat.h
@@ -1,11 +1,16 @@
 #ifndef COMBAT_H
 #define COMBAT_H
 
-#include "fourmis.h"
+#include "FourmisColonie.h"
 #include "structures.h"
 
-float calculerProbabiliteCoupMortel(int pvEnnemie, int pvDefenseur);  // Modification du type de retour en float
-void combatFourmis(Fourmi *Ennemie, Fourmi *soldat);
-void lancerCombat(Fourmi *colonie, int tailleColonie);
+// Initialisation des ennemis (prédateurs ou autres)
+void initialiserEnnemi(void *ennemi, int isPredateur);
+
+// Gestion du combat entre les soldats et les ennemis
+void combat(void *ennemi, Fourmi *soldat, Colonie *etatColonie, StockNourriture *stockNourriture, int isPredateur);
+
+// Lancement du combat pour toute la colonie
+void lancerCombat(Fourmi *colonie, int tailleColonie, Colonie *etatColonie, StockNourriture *stockNourriture);
 
 #endif
