3eb3d2daac34497d843a72ffafbb07e9f1a54791
Youcef9897 <fettal@et.esiea.fr>
2025-01-09 01:01:11 +0100

    Update climat.h

--- essie/climat.h
+++ essie/climat.h
@@ -1,11 +1,14 @@
-#ifndef CLIMAT_H
-#define CLIMAT_H
+// Protection contre les inclusions multiples du fichier d'en-tête
+#ifndef CLIMAT_H // Si CLIMAT_H n'est pas défini
+#define CLIMAT_H // Définir CLIMAT_H
 
-#include "structures.h"
+// Inclusion du fichier d'en-tête contenant les définitions des structures utilisées
+#include "structures.h" // Inclure le fichier structures.h
 
-// Déclaration des fonctions
-void definirClimat(Climat *climat);
-void afficherClimat(const Climat *climat);
-void avancerJour(Climat *climat);
+// Déclaration des fonctions pour gérer le climat
+void avancerJour(Climat *climat); // Déclaration de la fonction avancerJour qui prend un pointeur sur Climat
 
-#endif // CLIMAT_H
+void afficherClimat(const Climat *climat); // Déclaration de la fonction afficherClimat qui prend un pointeur constant sur Climat
+
+// Fin de la protection contre les inclusions multiples
+#endif // CLIMAT_H // Fin de la condition si CLIMAT_H n'est pas défini
